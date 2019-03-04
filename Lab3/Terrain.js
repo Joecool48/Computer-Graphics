76134865d7864ee8f2e2f6c3054cf7ee436a9b53
Joey Bahary <joey.bahary@gmail.com>
2019-03-04 01:31:32 -0600

    code written out for diamond square

--- Lab3/Terrain.js
+++ Lab3/Terrain.js
@@ -21,7 +21,7 @@ function min(a, b, c, d) {
 
 
 /** Class implementing 3D terrain. */
-class Terrain{   
+class Terrain{
 /**
  * Initialize members of a Terrain object
  * @param {number} div Number of triangles along x axis and y axis
@@ -36,7 +36,7 @@ class Terrain{
         this.minY=minY;
         this.maxX=maxX;
         this.maxY=maxY;
-        
+
         // Allocate vertex array
         this.vBuffer = [];
         // Allocate triangle array
@@ -46,20 +46,20 @@ class Terrain{
         // Allocate array for edges so we can draw wireframe
         this.eBuffer = [];
         console.log("Terrain: Allocated buffers");
-        
+
         this.generateTriangles();
         console.log("Terrain: Generated triangles");
-        
+
         this.generateLines();
         console.log("Terrain: Generated lines");
-        
+
         // Get extension for 4 byte integer indices for drwElements
         var ext = gl.getExtension('OES_element_index_uint');
         if (ext ==null){
             alert("OES_element_index_uint is unsupported by your browser and terrain generation cannot proceed.");
         }
     }
-    
+
     /**
     * Set the x,y,z coords of a vertex at location(i,j)
     * @param {Object} v an an array of length 3 holding x,y,z coordinates
@@ -90,20 +90,20 @@ class Terrain{
         v[1] = this.vBuffer[vid + 1];
         v[2] = this.vBuffer[vid + 2];
     }
-    
+
     /**
-    * Send the buffer objects to WebGL for rendering 
+    * Send the buffer objects to WebGL for rendering
     */
     loadBuffers()
     {
         // Specify the vertex coordinates
         this.VertexPositionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);      
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vBuffer), gl.STATIC_DRAW);
         this.VertexPositionBuffer.itemSize = 3;
         this.VertexPositionBuffer.numItems = this.numVertices;
         console.log("Loaded ", this.VertexPositionBuffer.numItems, " vertices");
-    
+
         // Specify normals to be able to do lighting calculations
         this.VertexNormalBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
@@ -112,8 +112,8 @@ class Terrain{
         this.VertexNormalBuffer.itemSize = 3;
         this.VertexNormalBuffer.numItems = this.numVertices;
         console.log("Loaded ", this.VertexNormalBuffer.numItems, " normals");
-    
-        // Specify faces of the terrain 
+
+        // Specify faces of the terrain
         this.IndexTriBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(this.fBuffer),
@@ -121,55 +121,55 @@ class Terrain{
         this.IndexTriBuffer.itemSize = 1;
         this.IndexTriBuffer.numItems = this.fBuffer.length;
         console.log("Loaded ", this.IndexTriBuffer.numItems, " triangles");
-    
-        //Setup Edges  
+
+        //Setup Edges
         this.IndexEdgeBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexEdgeBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(this.eBuffer),
                   gl.STATIC_DRAW);
         this.IndexEdgeBuffer.itemSize = 1;
         this.IndexEdgeBuffer.numItems = this.eBuffer.length;
-        
+
         console.log("triangulatedPlane: loadBuffers");
     }
-    
+
     /**
-    * Render the triangles 
+    * Render the triangles
     */
     drawTriangles(){
         gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize, 
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize,
                          gl.FLOAT, false, 0, 0);
 
         // Bind normal buffer
         gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
+        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute,
                            this.VertexNormalBuffer.itemSize,
-                           gl.FLOAT, false, 0, 0);   
-    
-        //Draw 
+                           gl.FLOAT, false, 0, 0);
+
+        //Draw
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
         gl.drawElements(gl.TRIANGLES, this.IndexTriBuffer.numItems, gl.UNSIGNED_INT,0);
     }
-    
+
     /**
-    * Render the triangle edges wireframe style 
+    * Render the triangle edges wireframe style
     */
     drawEdges(){
-    
+
         gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize, 
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize,
                          gl.FLOAT, false, 0, 0);
 
         // Bind normal buffer
         gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 
+        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute,
                            this.VertexNormalBuffer.itemSize,
-                           gl.FLOAT, false, 0, 0);   
-    
-        //Draw 
+                           gl.FLOAT, false, 0, 0);
+
+        //Draw
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexEdgeBuffer);
-        gl.drawElements(gl.LINES, this.IndexEdgeBuffer.numItems, gl.UNSIGNED_INT,0);   
+        gl.drawElements(gl.LINES, this.IndexEdgeBuffer.numItems, gl.UNSIGNED_INT,0);
     }
 
     getRandom(bias) {
@@ -177,52 +177,91 @@ class Terrain{
         if(Math.random() > .5) rand *= -1;
         return rand;
     }
-    // gets the new vertex value based on previous points indexes and bounds checks
-    getNewVertexValue(p1, p2, p3, p4, bias) {
-        var tot = 0;
-        var arr = [];
-        var avg_cnt = 0;
-        if (p1[0] < this.div && p1[0] >= 0 && p1[1] < this.div && p1[1] >= 0) {
-            avg_cnt += 1;
-            this.getVertex(arr, p1[1], p1[0]);
-            tot += arr[2];
+
+    // checks to make sure a point is in bounds and returns true if it is
+    in_bounds(pt) {
+        return pt[0] >= 0 && pt[0] < this.div && pt[1] < this.div && pt[1] >= 0;
+    }
+
+    // function to find the square corners by step size, average them, and set pt to that average plus a random bias
+    do_square(pt, step_size, bias) {
+        // first find all the nodes step size away horizontally and vertically
+        if (!this.in_bounds(pt)) console.log("Why would you give me this?");
+        var total = 0;
+        var count = 0;
+        var ret_pt = [];
+        var check_pt = [pt[0] + step_size, pt[1]];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2]; // add the z value to the total
+            count += 1;
         }
-        if (p2[0] < this.div && p2[0] >= 0 && p2[1] < this.div && p2[1] >= 0) {
-            avg_cnt += 1;
-            this.getVertex(arr, p2[1], p2[0]);
-            tot += arr[2];
+        check_pt = [pt[0] - step_size, pt[1]];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2];
+            count += 1;
         }
-        if (p3[0] < this.div && p3[0] >= 0 && p3[1] < this.div && p3[1] >= 0) {
-            avg_cnt += 1;
-            this.getVertex(arr, p3[1], p3[0]);
-            tot += arr[2];
+        check_pt = [pt[0], pt[1] + step_size];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2];
+            count += 1;
         }
-        if (p4[0] < this.div && p4[0] >= 0 && p4[1] < this.div && p4[1] >= 0) {
-            avg_cnt += 1;
-            this.getVertex(arr, p4[1], p4[0]);
-            tot += arr[2];
+        check_pt = [pt[0], pt[1] - step_size];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2];
+            count += 1;
         }
 
-        if (avg_cnt != 0) {
-            tot = (tot / avg_cnt) + this.getRandom(bias);
-            var vert = [];
-            var xmed = 0;
-            var ymed = 0;
-            // add the min x and min y to the max x and max y and find median
-            var xmax = max(p1[0], p2[0], p3[0], p4[0]);
-            var xmin = min(p1[0], p2[0], p3[0], p4[0]);
-            var ymax = max(p1[1], p2[1], p3[1], p4[1]);
-            var ymin = min(p1[1], p2[1], p3[1], p4[1]);
-
-            xmed = (xmax + xmin) / 2;
-            ymed = (ymax + ymin) / 2;
-            // get the old vertex x and y values
-            this.getVertex(vert, ymed, xmed);
-            this.setVertex([vert[0], vert[1], tot], ymed, xmed);
-            
+        if (count != 0) {
+            var new_pt_val = [];
+            this.getVertex(new_pt_val, pt[1], pt[0]);
+            var new_height = (total / count) + (Math.random() * bias);
+            this.setVertex([new_pt_val[0], new_pt_val[1], new_height], pt[1], pt[0]);
         }
 
     }
+    // function to get values at diamond angles, average them, and then assign point a random bias
+    do_diamond(pt, step_size, bias) {
+        if (!this.in_bounds(pt)) console.log("Why would you give me this?");
+        var total = 0;
+        var count = 0;
+        var ret_pt = [];
+        var check_pt = [pt[0] + step_size, pt[1] + step_size];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2]; // add the z value to the total
+            count += 1;
+        }
+        check_pt = [pt[0] - step_size, pt[1] - step_size];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2];
+            count += 1;
+        }
+        check_pt = [pt[0] - step_size, pt[1] + step_size];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2];
+            count += 1;
+        }
+        check_pt = [pt[0] + step_size, pt[1] - step_size];
+        if (this.in_bounds(check_pt)) {
+            this.getVertex(ret_pt, check_pt[1], check_pt[0]);
+            total += ret_pt[2];
+            count += 1;
+        }
+
+        if (count != 0) {
+            var new_pt_val = [];
+            this.getVertex(new_pt_val, pt[1], pt[0]);
+            var new_height = (total / count) + (Math.random() * bias);
+            this.setVertex([new_pt_val[0], new_pt_val[1], new_height], pt[1], pt[0]);
+        }
+    }
+
     diamondSquare() {
         // Set the starting vertices
 
@@ -242,24 +281,106 @@ class Terrain{
         var p2 = [this.div, 0];
         var p3 = [0, this.div];
         var p4 = [this.div, this.div];
-        var point_arr = [];
-        point_arr.push(p4);
-        point_arr.push(p3);
-        point_arr.push(p2);
-        point_arr.push(p1);
-        this.getNewVertexValue(p1, p2, p3, p4, 0);
+
         bias = .5;
-        //while (setCount !== this.numVertices) {
-            // Do the diamond step
-            
-        //}
+
+        var explored_set = new Set();
+        explored_set.add(p1);
+        explored_set.add(p2);
+        explored_set.add(p3);
+        explored_set.add(p4);
+
+        prev_added_points = [p1, p2, p3, p4];
+
+        var diamond_step = true;
+
+        var step_size = (this.div - 1) / 2;
+        // for every diamond step, take steps in every diagnal direction, and
+        // try to compute the midpoint there
+
+        while (prev_added_points.length > 0 && step_size > 0) {
+            if (diamond_step) {
+                // go through all the old points, and check them all for new diamond values to add
+                var len = prev_added_points.length;
+                for (var i = 0; i < len; i++) {
+                    var curr_pt = prev_added_points.pop();
+                    var new_pt = [curr_pt[0] + step_size, curr_pt[1] + step_size];
+                    if (this.in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        this.do_diamond(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                    new_pt = [curr_pt[0] + step_size, curr_pt[1] - step_size];
+                    if (this.in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        this.do_diamond(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                    new_pt = [curr_pt[0] - step_size, curr_pt[1] + step_size];
+                    if (this.in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        this.do_diamond(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                    new_pt = [curr_pt[0] - step_size, curr_pt[1] - step_size];
+                    if (this.in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        this.do_diamond(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                }
+                diamond_step = ~diamond_step;
+            }
+            /* otherwise it is a square step */
+            else {
+                var len = prev_added_points.length;
+                for (var i = 0; i < len; i++) {
+                    var curr_pt = prev_added_points.pop();
+                    var new_pt = [curr_pt[0] + step_size, curr_pt[1]];
+                    if (in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        do_square(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                    new_pt = [curr_pt[0] - step_size, curr_pt[1]];
+                    if (in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        do_square(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                    new_pt = [curr_pt[0], curr_pt[1] + step_size];
+                    if (in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        do_square(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+                    new_pt = [curr_pt[0], curr_pt[1] - step_size];
+                    if (in_bounds(new_pt) && !explored_set.has(new_pt)) {
+                        do_square(new_pt, step_size, bias);
+                        explored_set.add(new_pt);
+                        prev_added_points.push(new_pt);
+                    }
+
+                    diamond_step = ~diamond_step;
+                    step_size -= 1; // decrease step size
+                }
+
+                bias -= Math.random() * .1; // decrease the bias for tweaking terrain noise
+                diamond_step = ~diamond_step;
+                step_size -= 1;
+            }
+
+        }
+
+
+
     }
 
 
 
 /**
- * Fill the vertex and buffer arrays 
- */    
+ * Fill the vertex and buffer arrays
+ */
 generateTriangles()
 {
     //Your code here
@@ -301,23 +422,23 @@ generateTriangles()
  */
 printBuffers()
     {
-        
+
     for(var i=0;i<this.numVertices;i++)
           {
-           console.log("v ", this.vBuffer[i*3], " ", 
+           console.log("v ", this.vBuffer[i*3], " ",
                              this.vBuffer[i*3 + 1], " ",
                              this.vBuffer[i*3 + 2], " ");
-                       
+
           }
-    
+
       for(var i=0;i<this.numFaces;i++)
           {
-           console.log("f ", this.fBuffer[i*3], " ", 
+           console.log("f ", this.fBuffer[i*3], " ",
                              this.fBuffer[i*3 + 1], " ",
                              this.fBuffer[i*3 + 2], " ");
-                       
+
           }
-        
+
     }
 
 /**
@@ -332,13 +453,13 @@ generateLines()
         var fid=f*3;
         this.eBuffer.push(this.fBuffer[fid]);
         this.eBuffer.push(this.fBuffer[fid+1]);
-        
+
         this.eBuffer.push(this.fBuffer[fid+1]);
         this.eBuffer.push(this.fBuffer[fid+2]);
-        
+
         this.eBuffer.push(this.fBuffer[fid+2]);
         this.eBuffer.push(this.fBuffer[fid]);
     }
-    
+
 }
 }
